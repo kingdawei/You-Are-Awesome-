3f4bc69e1ed566eb684bbf5543e6b89e36fae82b
default <ykim@fa05956kyoung-tae.local>
2019-01-18 20:10:13 -0500

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Kim, Young-Tae on 2019. 1. 18..
+//  Copyright © 2019년 David Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
